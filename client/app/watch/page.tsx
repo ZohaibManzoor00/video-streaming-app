a66e3c3e8050bf4df5f03b97967ceb19f7dced30
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-08-19 12:08:07 -0400

    upload video working

--- client/app/watch/page.tsx
+++ client/app/watch/page.tsx
@@ -1,25 +1,27 @@
-'use client'
+"use client";
 
 import { useState, useEffect } from "react";
 import SignIn from "../_components/sign-in";
 import { onAuthStateChangedHelper } from "../_firebase/firebase";
 import { User } from "firebase/auth";
+import UploadVideo from "../_components/upload-video";
 
 export default function Watch() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedHelper(user => {
-        setUser(user)
-    })
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
 
-    return () => unsubscribe()
-  }, [])
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
-      <SignIn user={user}/>
+      <SignIn user={user} />
       <div>This is the watch page</div>
+      {user && <UploadVideo />}
     </>
   );
 }
