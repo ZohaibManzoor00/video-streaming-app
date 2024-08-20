e25247780aff5a1dbee8c9e74c7acb328007f0cf
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-08-20 19:49:38 -0400

    firebase config updated

--- client/app/firebase/firebase.ts
+++ client/app/firebase/firebase.ts
@@ -8,7 +8,6 @@ import {
 } from "firebase/auth";
 import { getFunctions } from "firebase/functions";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -16,7 +15,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }; 
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
